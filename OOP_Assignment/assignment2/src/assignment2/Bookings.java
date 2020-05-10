6836cfd1baa4ba3d71c482f37c294194c3ddc543
GeekyAndDangerous <gazby.jones@gmail.com>
2020-05-10 22:05:13 +1000

    OOP Assignment with Bookings Class

---
+++ OOP_Assignment/assignment2/src/assignment2/Bookings.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+/**
+ *
+ * @author Gazby
+ */
+public class Bookings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
