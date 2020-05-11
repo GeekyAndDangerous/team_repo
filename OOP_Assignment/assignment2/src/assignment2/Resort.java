5e41195996b4e4fa4ee5ab54c86dad47b060e88d
GeekyAndDangerous <gazby.jones@gmail.com>
2020-05-11 13:57:03 +1000

    Resort class updated

--- OOP_Assignment/assignment2/src/assignment2/Resort.java
+++ OOP_Assignment/assignment2/src/assignment2/Resort.java
@@ -5,10 +5,30 @@
  */
 package assignment2;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
  * @author Gazby
  */
 public class Resort {
+    private ArrayList <Booking> mBookings;
+    private ArrayList <Pet> mPets;
     
+    public boolean addBooking(LocalDate dateIn, LocalDate dateOut, int petNum){
+        boolean bookingAdded = false;
+        for (Pet p : mPets){
+            if (p.getPetNum == petNum){
+                    for (Booking b : mBookings){
+                        if (b.getPetNum == petNum){
+                            if (dateIn.compareTo(b.getDateIn()) >= 0 && dateOut.compareTo(b.getDateOut()) )
+                        }
+                    Booking booking = new Booking(dateIn, dateOut, petNum);
+                    mBookings.add(booking);
+                    }
+            }
+        }
+        return bookingAdded;
+    }
 }
