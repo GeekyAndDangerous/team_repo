ae085e47e22b2b6434094f4c6eec928efb5e7431
GeekyAndDangerous <gazby.jones@gmail.com>
2020-05-11 21:39:51 +1000

    Added sex enum class, pet class, petresortexception class and changes made to resort class

--- OOP_Assignment/assignment2/src/assignment2/Resort.java
+++ OOP_Assignment/assignment2/src/assignment2/Resort.java
@@ -7,6 +7,7 @@ package assignment2;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -16,19 +17,81 @@ public class Resort {
     private ArrayList <Booking> mBookings;
     private ArrayList <Pet> mPets;
     
-    public boolean addBooking(LocalDate dateIn, LocalDate dateOut, int petNum){
-        boolean bookingAdded = false;
-        for (Pet p : mPets){
-            if (p.getPetNum == petNum){
-                    for (Booking b : mBookings){
-                        if (b.getPetNum == petNum){
-                            if (dateIn.compareTo(b.getDateIn()) >= 0 && dateOut.compareTo(b.getDateOut()) )
-                        }
-                    Booking booking = new Booking(dateIn, dateOut, petNum);
-                    mBookings.add(booking);
-                    }
+//    public boolean addBooking(LocalDate dateIn, LocalDate dateOut, int petNum){
+//        boolean bookingAdded = false;
+//        for (Pet p : mPets){
+//            if (p.getPetNum() == petNum){
+//                    for (Booking b : mBookings){
+//                        if (b.getPetNum() == petNum){
+//                            if (dateIn.compareTo(b.getStartDate()) >= 0 && dateOut.compareTo(b.getEndDate()) )
+//                        }
+//                    Booking booking = new Booking(dateIn, dateOut, petNum);
+//                    mBookings.add(booking);
+//                    }
+//            }
+//        }
+//        return bookingAdded;
+//    }
+    public boolean isPet(int petNum){
+        boolean isPet = false;
+        for (Pet p : this.mPets) {
+            if (p.getPetNum() == petNum) {
+                isPet = true;
             }
         }
-        return bookingAdded;
+        return isPet;
+    }
+    
+    public boolean addPet(Pet newPet) {
+        boolean petAdded = false;
+        
+        if (! isPet(newPet.getPetNum())) {
+            mPets.add(newPet);
+            petAdded = true;
+        }
+        return petAdded;
+    }
+    
+    public Pet getPet(int petNum) {
+        Pet pet = null;
+
+        for (Pet p : this.mPets) {
+            if (p.getPetNum() == petNum) {
+                pet = p;
+            }
+        }
+        return pet;
+    }
+    
+    
+    public Pet[] getPets(){        
+        Pet[] petArray = this.mPets.toArray(new Pet[mPets.size()]);
+        Arrays.sort(petArray);
+        return petArray;
+    }
+//    
+//    public Pet[] getPets(String name){
+//        ArrayList <Pet> petsByName;
+//        
+//        for (Pet p: mPets){
+//            if (p.getPetName().equals(name)){
+//                petsByName.add(p);
+//            }
+//        }
+////        Pet[] petArray;
+////        petArray = 
+//        return petsByName.getPets();
+//    }
+    
+    public Booking[] getBookings(){
+        Booking[] bookings = this.mBookings.toArray(new Booking[mBookings.size()]);
+        Arrays.sort(bookings);
+        return bookings;
+    }
+    
+    public Booking[] getBookings(String name){
+        Booking[] bookings;
+        bookings = new Booking[1];
+        return bookings;
     }
 }
