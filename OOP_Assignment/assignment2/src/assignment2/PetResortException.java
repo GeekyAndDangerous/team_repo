977462a5c5fe2a64d754345cf143008dd1ee0ec5
GeekyAndDangerous <gazby.jones@gmail.com>
2020-05-11 21:44:24 +1000

    PetResortException added. Have not updated classes to reflect exception

---
+++ OOP_Assignment/assignment2/src/assignment2/PetResortException.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+/**
+ *
+ * @author Gazby
+ */
+public class PetResortException extends IllegalArgumentException {
+
+    /**
+     * Creates a new instance of <code>PetResortException</code> without detail
+     * message.
+     */
+    public PetResortException() {
+        super("Pet Resort Exception");
+    }
+
+    /**
+     * Constructs an instance of <code>PetResortException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public PetResortException(String msg) {
+        super(msg);
+    }
+}
