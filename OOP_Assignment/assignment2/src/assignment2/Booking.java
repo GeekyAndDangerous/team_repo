e2c28c18a093a764dd8dabf5d866d8bef0622285
Chris Owen <christophero@y7mail.com>
2020-05-11 04:06:07 +0000

    Booking.java v1 Updated to add methods and datamembers (should be working)

--- OOP_Assignment/assignment2/src/assignment2/Booking.java
+++ OOP_Assignment/assignment2/src/assignment2/Booking.java
@@ -5,17 +5,86 @@
  */
 package assignment2;
 
+import java.time.LocalDate;
+
 /**
- *
- * @author Gazby
+ * @version 1.0 
+ * @author Chris Owen
  */
 public class Booking {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    //Data Members
+    LocalDate mDateIn;
+    LocalDate mDateOut;
+    int mPetNum;
+    
+    public Booking(LocalDate dateIn,LocalDate dateOut,int petNum) {
+        if (dateIn.isAfter(LocalDate.now()) 
+                && dateOut.isAfter(dateIn)
+                && mPetNum > 0) {
+            mPetNum = petNum;
+            mDateIn = dateIn;
+            mDateOut = dateOut;
+        }
+        else {
+//            throw new PetResortException();
+        }
     }
     
+    /**
+     * Method returns the start date of the booking.
+     * @return LocalDate object containing the start date of the booking.
+     */
+    public LocalDate getStartDate() {
+        return mDateIn;
+    }
+    
+    /**
+     * Method returns the end date of the booking.
+     * @return LocalDate object containing the end date of the booking.
+     */
+    public LocalDate getEndDate() {
+        return mDateOut;
+    }
+    
+    public int getPetNum() {
+        return mPetNum;
+    }
+    
+    /**
+     * CompareTo method. 
+     * @return int containing the difference between two booking objects
+     */
+    public int compareTo(Object obj) {
+        Booking booking = (Booking) obj;
+        int difference = 0;
+        if (difference == 0) {
+            difference = mDateIn.compareTo(booking.getStartDate());
+            if (difference == 0) {
+                difference = mDateOut.compareTo(booking.getEndDate());
+                if (difference == 0) {
+                    difference = mPetNum - booking.getPetNum();
+                }
+            }
+        }
+        return difference;
+    }
+    
+    /**
+     * Equals class, compares this object against another and determining equality.
+     * @param obj2
+     * @return Boolean representing the equality of the two objects
+     */
+    @Override
+    public boolean equals(Object obj2) {
+        Booking booking = (Booking) obj2;
+        Boolean result = false;
+        if (mDateIn.equals(booking.getStartDate())
+                && mDateOut.equals(booking.getEndDate()) 
+                && mPetNum == booking.getPetNum()) {
+            
+        }
+        return result;
+    }
 }
+
