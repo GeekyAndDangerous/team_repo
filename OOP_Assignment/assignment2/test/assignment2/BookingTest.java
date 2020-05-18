8e248179930df53fe4a2a932eb913875dba66f1e
GeekyAndDangerous <gazby.jones@gmail.com>
2020-05-19 00:30:58 +1000

    Prepared for junit writing, not yet completed

---
+++ OOP_Assignment/assignment2/test/assignment2/BookingTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gazby
+ */
+public class BookingTest {
+    
+    public BookingTest() {
+    }
+
+    /**
+     * Test of getStartDate method, of class Booking.
+     */
+    @Test
+    public void testGetStartDate() {
+    }
+
+    /**
+     * Test of getEndDate method, of class Booking.
+     */
+    @Test
+    public void testGetEndDate() {
+    }
+
+    /**
+     * Test of getPetNum method, of class Booking.
+     */
+    @Test
+    public void testGetPetNum() {
+    }
+
+    /**
+     * Test of compareTo method, of class Booking.
+     */
+    @Test
+    public void testCompareTo() {
+    }
+
+    /**
+     * Test of equals method, of class Booking.
+     */
+    @Test
+    public void testEquals() {
+    }
+    
+}
---
+++ OOP_Assignment/assignment2/test/assignment2/ResortTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gazby
+ */
+public class ResortTest {
+    
+    public ResortTest() {
+    }
+
+    /**
+     * Test of addBooking method, of class Resort.
+     */
+    @Test
+    public void testAddBooking() {
+    }
+
+    /**
+     * Test of isPet method, of class Resort.
+     */
+    @Test
+    public void testIsPet() {
+    }
+
+    /**
+     * Test of addPet method, of class Resort.
+     */
+    @Test
+    public void testAddPet() {
+    }
+
+    /**
+     * Test of getPet method, of class Resort.
+     */
+    @Test
+    public void testGetPet() {
+    }
+
+    /**
+     * Test of getPets method, of class Resort.
+     */
+    @Test
+    public void testGetPets_0args() {
+    }
+
+    /**
+     * Test of getPets method, of class Resort.
+     */
+    @Test
+    public void testGetPets_String() {
+    }
+
+    /**
+     * Test of getBookings method, of class Resort.
+     */
+    @Test
+    public void testGetBookings_0args() {
+    }
+
+    /**
+     * Test of getBookings method, of class Resort.
+     */
+    @Test
+    public void testGetBookings_LocalDate() {
+    }
+
+    /**
+     * Test of toString method, of class Resort.
+     */
+    @Test
+    public void testToString() {
+    }
+    
+}
