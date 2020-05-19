6f7070f73b0f40a5c433960dec8acf02936ecc3f
GeekyAndDangerous <gazby.jones@gmail.com>
2020-05-19 12:13:03 +1000

    Created test class for Booking.java Has all available methods, no exception testing as yet

--- OOP_Assignment/assignment2/test/assignment2/BookingTest.java
+++ OOP_Assignment/assignment2/test/assignment2/BookingTest.java
@@ -5,8 +5,11 @@
  */
 package assignment2;
 
+import java.time.LocalDate;
+import java.time.Month;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
@@ -14,7 +17,11 @@ import static org.junit.Assert.*;
  */
 public class BookingTest {
     
-    public BookingTest() {
+    Booking b1;
+    
+    @Before
+    public void setUp(){
+        b1 = new Booking(LocalDate.of(2021, 7, 12), LocalDate.of(2021, 7, 15), 542);
     }
 
     /**
@@ -22,6 +29,9 @@ public class BookingTest {
      */
     @Test
     public void testGetStartDate() {
+        LocalDate expected = LocalDate.of(2021, 7, 12);
+        LocalDate actual = b1.getStartDate();
+        assertEquals(expected, actual);
     }
 
     /**
@@ -29,6 +39,9 @@ public class BookingTest {
      */
     @Test
     public void testGetEndDate() {
+        LocalDate expected = LocalDate.of(2021, 7, 15);
+        LocalDate actual = b1.getEndDate();
+        assertEquals(expected, actual);
     }
 
     /**
@@ -36,6 +49,9 @@ public class BookingTest {
      */
     @Test
     public void testGetPetNum() {
+        int expected = 542;
+        int actual = b1.getPetNum();
+        assertEquals(expected, actual);
     }
 
     /**
@@ -43,6 +59,10 @@ public class BookingTest {
      */
     @Test
     public void testCompareTo() {
+        Booking b2 = new Booking(LocalDate.of(2021, 7, 12), LocalDate.of(2021, 7, 15), 543);
+        int expected = -1;
+        int actual = b1.compareTo(b2);
+        assertEquals(expected, actual);
     }
 
     /**
@@ -50,6 +70,19 @@ public class BookingTest {
      */
     @Test
     public void testEquals() {
+        Booking b2 = new Booking(LocalDate.of(2021, 7, 12), LocalDate.of(2021, 7, 15), 542);
+        boolean expected = true;
+        boolean actual = b1.equals(b2);
+        assertEquals(expected, actual);
     }
     
+    /**
+     * Test of equals method, of class Booking.
+     */
+    @Test
+    public void testToString() {
+        String expected = "dateIn = 2021-07-12dateOut = 2021-07-15petNum = 542";
+        String actual = b1.toString().trim();
+        assertEquals(expected, actual);
+    }
 }
