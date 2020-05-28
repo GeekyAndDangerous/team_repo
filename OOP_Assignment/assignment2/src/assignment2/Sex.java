eea93e3344c372f08a76a90e9d114c9352c771ef
GeekyAndDangerous <gazby.jones@gmail.com>
2020-05-28 23:32:21 +1000

    Added extra details to authorship

--- OOP_Assignment/assignment2/src/assignment2/Sex.java
+++ OOP_Assignment/assignment2/src/assignment2/Sex.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package assignment2;
 
 /**
  *
  * @author Gareth Jones, Chris Owen, Connor Wesley-Harvey
+ * @version 1.0
+ * CSE2OOP
+ * Tutor: Akhtar Ali Jalbani
+ * Assignment 2
+ * 
  */
+
 public enum Sex {
     FEMALE, MALE 
 }
